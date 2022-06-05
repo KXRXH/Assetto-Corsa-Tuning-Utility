264f09fba2cd70d9d19991d65cbf662c40c85d08
KXRXH <kirayfolon@gmail.com>
2022-06-05 23:20:03 +0700

    new feature

--- AssettoCorsaTuningUtility/Form1.Designer.cs
+++ AssettoCorsaTuningUtility/Form1.Designer.cs
@@ -45,6 +45,24 @@ namespace AssettoCorsaTuningUtility
             this.DriveTrainPage = new System.Windows.Forms.TabPage();
             this.drivetrainTextBox = new System.Windows.Forms.RichTextBox();
             this.EngineSwapPage = new System.Windows.Forms.TabPage();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.Comment1 = new System.Windows.Forms.Label();
             this.CarBox2 = new System.Windows.Forms.ComboBox();
             this.TuneThisBtn = new System.Windows.Forms.Button();
@@ -169,6 +187,24 @@ namespace AssettoCorsaTuningUtility
             // 
             // EngineSwapPage
             // 
+            this.EngineSwapPage.Controls.Add(this.textBox8);
+            this.EngineSwapPage.Controls.Add(this.textBox7);
+            this.EngineSwapPage.Controls.Add(this.textBox6);
+            this.EngineSwapPage.Controls.Add(this.textBox5);
+            this.EngineSwapPage.Controls.Add(this.textBox4);
+            this.EngineSwapPage.Controls.Add(this.textBox3);
+            this.EngineSwapPage.Controls.Add(this.textBox2);
+            this.EngineSwapPage.Controls.Add(this.textBox1);
+            this.EngineSwapPage.Controls.Add(this.label13);
+            this.EngineSwapPage.Controls.Add(this.label14);
+            this.EngineSwapPage.Controls.Add(this.label11);
+            this.EngineSwapPage.Controls.Add(this.label12);
+            this.EngineSwapPage.Controls.Add(this.label9);
+            this.EngineSwapPage.Controls.Add(this.label10);
+            this.EngineSwapPage.Controls.Add(this.label8);
+            this.EngineSwapPage.Controls.Add(this.label7);
+            this.EngineSwapPage.Controls.Add(this.label6);
+            this.EngineSwapPage.Controls.Add(this.label5);
             this.EngineSwapPage.Controls.Add(this.Comment1);
             this.EngineSwapPage.Controls.Add(this.CarBox2);
             this.EngineSwapPage.Enabled = false;
@@ -180,9 +216,174 @@ namespace AssettoCorsaTuningUtility
             this.EngineSwapPage.Text = " Engine Swap";
             this.EngineSwapPage.UseVisualStyleBackColor = true;
             // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(439, 190);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.ShortcutsEnabled = false;
+            this.textBox8.Size = new System.Drawing.Size(79, 20);
+            this.textBox8.TabIndex = 27;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(439, 157);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(79, 20);
+            this.textBox7.TabIndex = 26;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(439, 124);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(79, 20);
+            this.textBox6.TabIndex = 25;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(439, 91);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(79, 20);
+            this.textBox5.TabIndex = 24;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(109, 190);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(79, 20);
+            this.textBox4.TabIndex = 23;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(109, 157);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(79, 20);
+            this.textBox3.TabIndex = 22;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(109, 124);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(79, 20);
+            this.textBox2.TabIndex = 21;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(109, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(79, 20);
+            this.textBox1.TabIndex = 20;
+            // 
+            // label13
+            // 
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label13.Location = new System.Drawing.Point(347, 189);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(99, 21);
+            this.label13.TabIndex = 19;
+            this.label13.Text = "Turbo";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label14
+            // 
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label14.Location = new System.Drawing.Point(15, 189);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(99, 21);
+            this.label14.TabIndex = 18;
+            this.label14.Text = "Turbo";
+            this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label11.Location = new System.Drawing.Point(347, 156);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(99, 21);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Idle RPM";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label12.Location = new System.Drawing.Point(15, 156);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(99, 21);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "Idle RPM";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label9.Location = new System.Drawing.Point(347, 123);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(99, 21);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Max RPM";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label10.Location = new System.Drawing.Point(15, 123);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(99, 21);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Max RPM";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label8.Location = new System.Drawing.Point(347, 90);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 21);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Max torque";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label7.Location = new System.Drawing.Point(15, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 21);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Max torque";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label6.Location = new System.Drawing.Point(360, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "New engine";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label5.Location = new System.Drawing.Point(40, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Old engine";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
             // Comment1
             // 
-            this.Comment1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle) ((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.Comment1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle) ((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte) (204)));
             this.Comment1.Location = new System.Drawing.Point(15, 3);
             this.Comment1.Name = "Comment1";
             this.Comment1.Size = new System.Drawing.Size(538, 23);
@@ -316,10 +517,35 @@ namespace AssettoCorsaTuningUtility
             this.EnginePage.ResumeLayout(false);
             this.DriveTrainPage.ResumeLayout(false);
             this.EngineSwapPage.ResumeLayout(false);
+            this.EngineSwapPage.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
-        
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+
+        private System.Windows.Forms.Label label7;
+
+        private System.Windows.Forms.Label label6;
+
+        private System.Windows.Forms.Label label5;
+
         private System.Windows.Forms.TextBox newTuneNameTextBox;
 
         private System.Windows.Forms.Button createNewCarBtn;
--- AssettoCorsaTuningUtility/Form1.cs
+++ AssettoCorsaTuningUtility/Form1.cs
@@ -109,5 +109,6 @@ namespace AssettoCorsaTuningUtility
         {
             StartTuningEvent();
         }
+        
     }
 }
